79878531285157d44a4d0fbeadeb13ac9a235fcc
Raviraj Subramanian <raviraj.subramanian@freshworks.com>
2024-04-02 19:34:28 +0530

    [DEVREL-3846] Migrate to platform v3.0

--- conditional-fields/visitor-app/scripts/app.js
+++ conditional-fields/visitor-app/scripts/app.js
@@ -11,33 +11,33 @@ function getPortalType(){
 }
 
 async function onAppActivate() {
-  
   try {
     const iparams = await getIparams();
-        try {
+    try {
       const portalData = await getPortalType();
-              if (portalData.portal.host === `${iparams.domain}.freshdesk.com`){
-          console.info(`The current portal is ${iparams.domain}.freshdesk.com`);
+      const domain = iparams.domain.includes("https://") ? iparams.domain.split('https://')[1] : iparams.domain; // Check if the domain iparam has the protocol prefix. If so, remove it.
+      if (portalData.portal.host === domain){
+        console.info(`The current portal is ${domain}`);
+        try {
+          await hideTicketField("description_html");
+          console.info("The Description field is hidden");
           try {
-            await hideTicketField("description_html");
-            console.info("The Description field is hidden");
-            try {
-            await client.interface.trigger("setValue", {id: iparams.custom_field_to_set, value: iparams.custom_field_to_set_value});
-              console.info("The configured field is set with the configured value");
-            } catch (error) {
-              console.error("Error: Failed to set value");
-              console.error(error);
-            }
-          } catch(error){
-            console.error("Error: Failed to hide ticket field in new ticket form");
+          await client.interface.trigger("setValue", {id: iparams.custom_field_to_set, value: iparams.custom_field_to_set_value});
+            console.info("The configured field is set with the configured value");
+          } catch (error) {
+            console.error("Error: Failed to set value");
             console.error(error);
           }
-        } else{
-          console.info("Skipped customizations for portal other than the configured portal");
+        } catch(error){
+          console.error("Error: Failed to hide ticket field in new ticket form");
+          console.error(error);
         }
+      } else{
+        console.info("Skipped customizations for portal other than the configured portal");
+      }
     } catch (error) {
-        console.error("Error: Failed to get portal type");
-        console.error(error);
+      console.error("Error: Failed to get portal type");
+      console.error(error);
     }
   } catch (error) {
     console.error("Error: Failed to get iparams");
